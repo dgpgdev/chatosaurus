8f17a2243a76b1a0bd184a8b43c22cb1f7837fe8
gauthier <dgpgdev@gmail.com>
2022-10-24 09:43:56 +0200

    add test

--- tests/Middleware_test.ts
+++ tests/Middleware_test.ts
@@ -1,11 +1,19 @@
-import {
-  assertEquals,
-  assertExists,
-  assertInstanceOf,
-} from 'https://deno.land/std@0.158.0/testing/asserts.ts'
-import { WebSocketServer } from '../ws/WebSocketServer.ts'
+import { assertEquals } from 'https://deno.land/std@0.158.0/testing/asserts.ts'
 
-Deno.test(`Room Manager no room at start`, () => {
-  const server = new WebSocketServer()
-  server.use()
+import { Context, WebSocketUser, Next } from '../mod.ts'
+import MiddleWareManager from '../src/middleware/MiddleWareManager.ts'
+
+Deno.test(`Test Middleware`, () => {
+  const mdwm = new MiddleWareManager()
+  const context: Context = {
+    evt: 'some',
+    ws: { id: '4444' } as WebSocketUser,
+    data: [0],
+  }
+  mdwm.use((ctx, next) => {
+    ;(ctx.data[0] as number) += 1
+    next()
+  })
+
+  assertEquals(context.data[0], 1)
 })
