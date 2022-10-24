fe1f3ccdb087c78468f52a8cc9a20de38bd2c20a
gauthier <dgpgdev@gmail.com>
2022-10-24 15:31:47 +0200

    fix comment remove console

--- server.ts
+++ server.ts
@@ -1,6 +1,19 @@
-import { WebSocketServer } from './mod.ts'
+import { WebSocketServer, WebSocketUser } from './mod.ts'
 
 const connectInfo = { hostname: 'localhost', port: 8080 }
 
 const server = new WebSocketServer(connectInfo)
+
+server.on('join', (user: WebSocketUser, roomId: string) => {
+  user.join(roomId)
+})
+
+server.on('leave', (user: WebSocketUser, roomId: string) => {
+  user.leave(roomId)
+})
+
+server.on('leaveAll', (user: WebSocketUser, roomId: string) => {
+  user.leaveAll
+})
+
 server.start()
