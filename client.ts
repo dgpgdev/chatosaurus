6690fd86b469b940c386c40f6ad6713078f5aaea
gauthier <dgpgdev@gmail.com>
2022-11-15 16:18:01 +0100

    add client and server demo

---
+++ client.ts
@@ -0,0 +1,16 @@
+import { WebSocketClient } from './mod.ts'
+
+const client = new WebSocketClient('ws://localhost:4000', {
+  reconnect: true,
+  delay: 5000,
+})
+
+client.on('error', (err) => console.log(err.message))
+
+client.on('broadcastedEvent', (message) => {
+  console.log(message)
+})
+
+client.on('myClientEvent', (message) => console.log(message))
+
+client.connect()
