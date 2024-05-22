7702e963870df6bd8004340142a908dcb6d081c8
Gauthier <gauthier@swello.com>
2024-05-22 10:04:49 +0200

    fix: execute middleware

--- tests/Middleware_test.ts
+++ tests/Middleware_test.ts
@@ -1,19 +1,19 @@
-import { assertEquals } from 'https://deno.land/std@0.158.0/testing/asserts.ts'
+import { assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts"
 
-import { Context, WebSocketUser, Next } from '../mod.ts'
-import MiddleWareManager from '../src/middleware/MiddleWareManager.ts'
+import { Context, WebSocketUser, Next } from "../mod.ts"
+import MiddleWareManager from "../src/middleware/MiddleWareManager.ts"
 
 Deno.test(`Test Middleware`, () => {
   const mdwm = new MiddleWareManager()
   const context: Context = {
-    evt: 'some',
-    ws: { id: '4444' } as WebSocketUser,
+    evt: "some",
+    ws: { id: "4444" } as WebSocketUser,
     data: [0],
   }
   mdwm.use((ctx, next) => {
     ;(ctx.data[0] as number) += 1
     next()
   })
-
+  mdwm.executeMiddleware(context)
   assertEquals(context.data[0], 1)
 })
